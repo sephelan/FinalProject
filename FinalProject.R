1e8079e02b47d0c9a2b878f5adabbdacfee73af4
anjtaylo <anjtaylo@mtu.edu>
2023-11-09 14:27:43 -0500

    leverages,fittedvalues, residuals, and plots

--- FinalProject.R
+++ FinalProject.R
@@ -1,11 +1,13 @@
 
-
+rm( list = ls())
 setwd("/Users/seanphelan/Desktop/Regression")
 stream = read.csv('streamflow.csv')
 stream$STAID <- NULL
 
-library(car)
-
+library('car')
+library('pracma')
+library('moments')
+library('DescTools')
 ######## exploratory data analysis #######
 summary(stream)
 
@@ -37,3 +39,34 @@ cor_stream
 
 addVarPlotModel <- lm(data=stream,max90~DRAIN_SQKM+PPTAVG_BASIN+T_AVG_BASIN+T_AVG_SITE+RH_BASIN+MAR_PPT7100_CM+RRMEDIAN)
 avPlots(addVarPlotModel)
+
+#  Basic LM of our project and leverage, fitted values, and residual plots
+library('car')
+n = 293
+regstream <- lm(max90 ~ DRAIN_SQKM + PPTAVG_BASIN + T_AVG_BASIN + T_AVG_SITE + RH_BASIN +  MAR_PPT7100_CM + RRMEDIAN, stream)
+regstream_matrix <- as.matrix(regstream)
+Y <- matrix(stream[,1], ncol = 1)
+X <- as.matrix(stream[,-1])
+onevec <- matrix(rep(1,n), ncol = 1)
+X <- cbind(onevec,X)
+X <- as.matrix(X)
+inv.XX <- solve(t(X)%*%X)
+H <- X%*%inv.XX%*%t(X)
+#  leverages
+lev <- diag(H)
+#  Residuals
+res_stream <- rstudent(regstream)
+#  fitted values
+fittedvalues_stream <- fitted(regstream)
+
+#  residual plot against time
+par(mfrow=c(1,1))
+plot(res_stream , xlab="time", ylab="residual", main="Plot of residual against time")
+
+#  residual plots against fitted values:
+plot(res_stream~fittedvalues_stream, xlab="fitted values", ylab="residual", 
+     main="Plot of residuals against fitted values")
+
+#  residual plots against predictor values: 
+plot(res_stream~stream$DRAIN_SQKM, xlab="Violent predictor", ylab="residual", 
+     main="Plot of residuals against Violent predictor")
